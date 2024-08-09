20b50343dd6ef33e8f7567c4fa5e9bc4a4f78419
EnesAkyuz <eakyuz0681@gmail.com>
2024-08-08 18:08:39 -0700

    Url bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useParams } from 'react-router-dom';  // Import useParams here
 import HomePage from './pages/HomePage';
 import AddCreator from './components/AddCreator';
 import CardDetail from './pages/CardDetail';
@@ -11,13 +11,18 @@ const App: React.FC = () => {
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/add" element={<AddCreator />} />
-        <Route path="/edit/:id" element={<AddCreator />} />
+        <Route path="/add" element={<AddCreator id={null} />} />
+        <Route path="/edit/:id" element={<AddCreatorWrapper />} />
         <Route path="/card/:id" element={<CardDetail />} />
       </Routes>
     </div>
   );
 };
 
-export default App;
+// Wrapper to extract id from route params and pass it to AddCreator
+const AddCreatorWrapper: React.FC = () => {
+  const { id } = useParams<{ id: string }>(); // Get the id from the route params
+  return <AddCreator id={id || null} />;
+};
 
+export default App;
